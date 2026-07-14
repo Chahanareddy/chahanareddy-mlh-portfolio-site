daefc488a6c61acff4cff0e6a0859f3dd89f10b3
Chahana Reddy <chahanareddy18@gmail.com>
2026-07-13 22:31:57 -0400

    update script to use systemd

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-tmux kill-server 2>/dev/null
-
 cd ~/chahanareddy-mlh-portfolio-site
 
 git fetch
@@ -10,4 +8,4 @@ git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s flask "cd ~/chahanareddy-mlh-portfolio-site && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+sudo systemctl restart myportfolio
